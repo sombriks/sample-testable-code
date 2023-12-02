ec7d55fee8b5589d16a559b121cbcad4b7bc96f8
Leonardo Silveira <sombriks@gmail.com>
2023-12-02 20:38:32 -0300

    sampling environment aware thing

--- 03-environment-aware/app/app.test.mjs
+++ 03-environment-aware/app/app.test.mjs
@@ -1,7 +1,22 @@
 import test from 'ava'
 import request from 'supertest'
 
+import { db } from './database.mjs'
 import { app } from './main.mjs'
+
+test.before(async t => {
+  await db.raw(`
+  create table books(
+    id integer not null primary key autoincrement,
+    title text not null,
+    author text not null
+  );
+  
+  insert into books (title, author) values ('American Gods', 'Neil Gaiman');
+  insert into books (title, author) values ('Sandman', 'Neil Gaiman');
+  insert into books (title, author) values ('Watchmen', 'Alan Moore');  
+  `)
+})
 
 test('should get books', async t => {
   const result = await request(app.callback())
--- 03-environment-aware/app/database.mjs
+++ 03-environment-aware/app/database.mjs
@@ -1,7 +1,12 @@
 import Knex from 'knex'
 
-export const db = Knex({
+const config = 'test' === process.env.NODE_ENV ? {
+  connection: ':memory:',
+  client: 'sqlite3'
+} : {
   connection: process.env.PG_CONNECTION_URL,
   pool: { min: 1, max: 2 },
   client: 'pg'
-})
+}
+
+export const db = Knex(config)
